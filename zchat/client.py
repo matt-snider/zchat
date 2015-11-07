80d19acfe7107f28689a29b459d29e94c7152548
Matt Snider <matt.snider@alum.utoronto.ca>
2015-11-08 00:04:43 +0100

    Write a simple client that sends messages to server

---
+++ zchat/client.py
@@ -0,0 +1,29 @@
+import sys
+import zmq
+
+
+def run_chat_client(user, hostname, port):
+    context = zmq.Context()
+    socket = context.socket(zmq.DEALER)
+    socket.identity = user.decode()
+    socket.connect('tcp://{}:{}'.format(hostname, port))
+
+    print('Please enter a command...')
+    try:
+        while True:
+            cmd = input()
+            # parse and validate
+            socket.send(cmd.encode())
+            response = socket.recv_multipart()
+            print('Response: %s' % response)
+
+    except KeyboardInterrupt:
+        exit('Exiting zchat...')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        exit('Usage: python -m zchat.client <user> <hostname> <port>')
+
+    user, hostname, port = sys.argv[1:]
+    run_chat_client(user, hostname, port)
