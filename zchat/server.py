3bb6cc86766b92d61bd6154a425f8e7db9db2d70
Matt Snider <matt.snider@alum.utoronto.ca>
2015-11-08 00:23:45 +0100

    Add logging to server

--- zchat/server.py
+++ zchat/server.py
@@ -1,11 +1,19 @@
 import sys
+import logging
+
 import zmq
+
+
+logging.basicConfig(format='%(asctime)-15 %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 def run_chat_server(port):
     context = zmq.Context()
     socket = context.socket(zmq.ROUTER)
     socket.bind('tcp://*:%s' % port)
+    logger.info('Server started on port %s' % port)
 
     try:
         while True:
@@ -16,11 +24,12 @@ def run_chat_server(port):
 
 
 def handle_command(user, cmd):
-    print("User '%s' sent command '%s'" % (user, cmd))
+    logger.debug("User<%s> sent command: %s" % (user, cmd))
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         exit('Usage: python -m zchat.server <port>')
 
+    logger.info('Starting zchat server...')
     run_chat_server(sys.argv[1])
