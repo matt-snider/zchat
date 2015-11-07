30fb54b5c1a3d0c46bd2e9e583f62f446dfea3a1
Matt Snider <matt.snider@alum.utoronto.ca>
2015-11-07 23:29:55 +0100

    Write main server loop that simply prints user request

---
+++ zchat/server.py
@@ -0,0 +1,25 @@
+import sys
+import zmq
+
+
+def run_chat_server(port):
+    context = zmq.Context()
+    socket = context.socket(zmq.ROUTER)
+
+    try:
+        while True:
+            user, cmd = socket.recv_multipart()
+            handle_command(user, cmd)
+    except KeyboardInterrupt:
+        exit('Closing server.')
+
+
+def handle_command(user, cmd):
+    print("User '%s' sent command '%s'" % (user, cmd))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        exit('Usage: python -m zchat.server <port>')
+
+    run_chat_server(sys.argv[1])
