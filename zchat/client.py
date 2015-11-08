3a2d0dd731d944274a3301539f7dadde8b8c98ce
Matt Snider <matt.snider@alum.utoronto.ca>
2015-11-08 17:54:13 +0100

    Run client-side commands through CommandRegistry

--- zchat/client.py
+++ zchat/client.py
@@ -1,50 +1,28 @@
 import sys
 import zmq
 
+from zchat.commands import CommandRegistry
+
 context = zmq.Context()
 
 
 class ZChatClient:
 
-    def __init__(self, user, hostname, port):
-        self.url = 'tcp://{}:{}'.format(hostname, port)
+    def __init__(self):
         self.socket = context.socket(zmq.DEALER)
-        self.socket.identity = user.encode()
-        self.commands = set()
+        self.registry = CommandRegistry('client', self.socket)
 
     def run(self):
-        self.socket.connect(self.url)
-        self.socket.send(b'REGISTER')
-        welcome = self.socket.recv_string()
-        print(welcome)
-
         print('Please enter a command...')
         while True:
-            cmd = self.parse_and_validate_command(input())
-            if not cmd:
-                continue
-            self.socket.send(cmd.encode())
-            response = self.socket.recv_multipart()
-            print('Response: %s' % response)
-
-    def parse_and_validate_command(self, cmd):
-        if not cmd.startswith('/'):
-            print("Prefix commands with '/' -- type /help for more info")
-            return
-
-        cmd = cmd.strip('/')
-        if cmd not in self.commands:
-            print("Invalid command: '%s' -- type /help for a list of commands" % cmd)
-            return
-        return cmd
+            try:
+                self.registry.dispatch(input())
+            except:
+                print("Invalid command -- type /help for more info")
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        exit('Usage: python -m zchat.client <user> <hostname> <port>')
-
-    user, hostname, port = sys.argv[1:]
-    client = ZChatClient(user, hostname, port)
+    client = ZChatClient()
     try:
         client.run()
     except KeyboardInterrupt:
