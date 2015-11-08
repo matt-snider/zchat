805c3c30d71c60dade3f9ac18d13d756c9d1561e
Matt Snider <matt.snider@alum.utoronto.ca>
2015-11-08 01:33:05 +0100

    Refactor server loop into ZChatServer class

--- zchat/server.py
+++ zchat/server.py
@@ -3,45 +3,51 @@ import logging
 
 import zmq
 
-
 logging.basicConfig(format='%(asctime)-15s %(message)s')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-welcome = b"""Welcome to zchat server!
-
-This is a welcome message that needs to be updated with something
-more interesting. Hello world! Foo bar biz baz bam!
-"""
-clients = []
+context = zmq.Context()
 
 
-def run_chat_server(port):
-    context = zmq.Context()
-    socket = context.socket(zmq.ROUTER)
-    socket.bind('tcp://*:%s' % port)
-    logger.info('Server started on port %s', port)
+class ZChatServer:
 
-    try:
-        while True:
-            user, cmd = socket.recv_multipart()
-            result = handle_command(user, cmd)
-            socket.send_multipart([user, result])
-    except KeyboardInterrupt:
-        exit('Closing server.')
+    welcome = b"""Welcome to zchat server!
 
+    This is a welcome message that needs to be updated with something
+    more interesting. Hello world! Foo bar biz baz bam!
+    """
 
-def handle_command(user, cmd):
-    logger.debug("User<%s> sent command: %s" % (user, cmd))
-    if cmd == b'REGISTER':
-        clients.append(user)
-        return welcome
-    else:
-        return cmd
+    def __init__(self, port):
+        self.socket = context.socket(zmq.ROUTER)
+        self.port = port
+        self.clients = []
+
+    def start(self):
+        self.socket.bind('tcp://*:{}'.format(self.port))
+        logger.info('Server started on port %s', self.port)
+
+        try:
+            while True:
+                user, cmd = self.socket.recv_multipart()
+                result = self.handle_command(user, cmd)
+                self.socket.send_multipart([user, result])
+        except KeyboardInterrupt:
+            exit('Closing server.')
+
+    def handle_command(self, user, cmd):
+        logger.debug("User<%s> sent command: %s" % (user, cmd))
+        if cmd == b'REGISTER':
+            self.clients.append(user)
+            return self.welcome
+        else:
+            return cmd
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         exit('Usage: python -m zchat.server <port>')
 
     logger.info('Starting zchat server...')
-    run_chat_server(sys.argv[1])
+    server = ZChatServer(sys.argv[1])
+    server.start()
