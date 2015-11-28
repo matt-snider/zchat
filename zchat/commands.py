5c1368af0403c124e3939a8d515e9c11114ca508
Matt Snider <matt.snider@alum.utoronto.ca>
2015-11-28 15:19:24 +0100

    Refactor commands to implement client() & server() and accept self

--- zchat/commands.py
+++ zchat/commands.py
@@ -65,11 +65,11 @@ class Command(ABC):
         return cls.get_help().split('\n', 1)[0].strip()
 
     @abstractmethod
-    def execute_server(cls, server, user, *args):
+    def server(self, user, *args):
         pass
 
     @abstractmethod
-    def execute_client(cls, client, *args):
+    def client(self, *args):
         pass
 
 
@@ -83,19 +83,17 @@ class Connect(Command):
         /connect <host> <nick>
     """
 
-    @classmethod
-    def execute_client(cls, client, host, nick):
-        client.socket.identity = nick.encode()
-        client.socket.connect('tcp://{}'.format(host))
-        client.socket.send(b'CONNECT')
-        welcome = client.socket.recv_string()
-        client.print_server_response(welcome)
+    def client(self, host, nick):
+        self.socket.identity = nick.encode()
+        self.socket.connect('tcp://{}'.format(host))
+        self.socket.send(b'CONNECT')
+        welcome = self.socket.recv_string()
+        self.print_server_response(welcome)
 
-    @classmethod
-    def execute_server(cls, server, user):
+    def server(self, user):
         print('%s connected' % user)
-        server.socket.send_multipart([user, server.welcome])
-        server.clients.append(user.decode())
+        self.socket.send_multipart([user, self.welcome])
+        self.clients.append(user.decode())
 
 
 @CommandRegistry.register
@@ -108,17 +106,15 @@ class Users(Command):
         /users 
     """
 
-    @classmethod
-    def execute_client(cls, client):
-        client.socket.send(b'USERS')
-        users = client.socket.recv_json()
-        client.print_server_response('Users:\n\t%s' % '\n\t'.join(users))
+    def client(self):
+        self.socket.send(b'USERS')
+        users = self.socket.recv_json()
+        self.print_server_response('Users:\n\t%s' % '\n\t'.join(users))
 
-    @classmethod
-    def execute_server(cls, server, user):
-        # this wqont work without addr!
-        user_list = json.dumps(server.clients).encode()
-        server.socket.send_multipart([user, user_list])
+    def server(self, user):
+        # this wont work without addr!
+        user_list = json.dumps(self.clients).encode()
+        self.socket.send_multipart([user, user_list])
 
 
 @CommandRegistry.register
@@ -132,19 +128,17 @@ class Message(Command):
     """
     
     # TODO: this would be much better with ZMQStream and on_recv()/on_send()
-    @classmethod
-    def execute_client(cls, client, target, msg):
-        me = client.socket.identity.decode()
+    def client(self, target, msg):
+        me = self.socket.identity.decode()
         print('<%s> %s' % (me, msg))
         while True:
-            client.socket.send(b'PRIVMSG ' + target.encode() + b' :' + msg.encode())
-            response = client.socket.recv_string()
+            self.socket.send(b'PRIVMSG ' + target.encode() + b' :' + msg.encode())
+            response = self.socket.recv_string()
             print('<%s> %s' % (target, response)) 
             msg = input('<%s> '  % me)
 
-    @classmethod
-    def execute_server(cls, server, user, target, msg):
-        server.socket.send_multipart([target.encode(), msg.encode()])
+    def server(self, user, target, msg):
+        self.socket.send_multipart([target.encode(), msg.encode()])
 
 
 @CommandRegistry.register
@@ -157,12 +151,11 @@ class Help(Command):
         /help <command_name>
     """
 
-    @classmethod
-    def execute_client(cls, user, command=None):
+    def client(self, command=None):
         if command:
-            cls._print_command_help(command)
+            self._print_command_help(command)
         else:
-            cls._print_command_list()
+            self._print_command_list()
 
     @classmethod
     def _print_command_help(cls, command):
