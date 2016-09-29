f50549cc3c915a32a12b563924e1add197d5b06c
Matt Snider <matt.snider@alum.utoronto.ca>
2016-09-30 00:14:25 +0200

    Remove unneeded methods/imports

--- zchat/client.py
+++ zchat/client.py
@@ -1,6 +1,4 @@
 import asyncio
-import shutil
-import textwrap
 
 import zmq
 import zmq.asyncio
@@ -19,9 +17,6 @@ class ZChatClient(CommandRegistry):
     def __init__(self):
         super().__init__()
         self.socket = ctx.socket(zmq.DEALER)
-        self._wrapper = textwrap.TextWrapper(replace_whitespace=False,
-                                             initial_indent=self.resp_prefix,
-                                             subsequent_indent=self.resp_prefix)
 
     @asyncio.coroutine
     def run_command(self, command):
@@ -35,45 +30,8 @@ class ZChatClient(CommandRegistry):
         except InvalidArgument:
             return 'Invalid arguments to command'
 
-    @asyncio.coroutine
-    def run(self):
-        print('Please enter a command...')
-        while True:
-            try:
-                user_input = input(self.prompt)
-                if not user_input:
-                    continue
-                self.dispatch(user_input)
-            except InvalidCommand:
-                print("Invalid command -- type /help for more info")
-            except InvalidArgument:
-                print('Invalid arguments to command')
-
-    @asyncio.coroutine
-    def receive(self):
-        while True:
-            cmd_name, *message = yield from self.socket.recv_multipart()
-            command = self._commands[cmd_name.decode().upper()]
-            command.on_message(self, [arg.decode() for arg in message])
-
     def execute_command(self, command, *args):
         return command.client(self, *args)
-
-    def on_message(self, message):
-        cmd_name, *message = message
-        command = self._commands[cmd_name.decode().upper()]
-        command.on_message(self, [arg.decode() for arg in message])
-
-    def print_server_response(self, response):
-        print('\n')
-        width, _ = shutil.get_terminal_size()
-        self._wrapper.width = width - len(self.resp_prefix)
-        for line in response.splitlines():
-            if not line:
-                print(self.resp_prefix)
-            else:
-                print(self._wrapper.fill(line))
-        print(self.prompt)
 
 
 if __name__ == '__main__':
